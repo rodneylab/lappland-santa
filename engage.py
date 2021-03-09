462febfc1bf1c26b8280a9e1b6789949ef302f07
Rodney Lab Codesign <codesign@rodneylab.com>
2021-03-09 14:06:36 +0000

    feat(engage.py): ✨ updated engage.py to write system properties to files also linted file

--- engage.py
+++ engage.py
@@ -30,11 +30,10 @@ def set_directory(path: Path):
         os.chdir(origin)
 
 
-def get_random_server_name():
-    result = choices(string.ascii_lowercase, k=3)
-    for i in range(0, 7):
-        result.append(str(randint(0, 9)))
-    return ''.join(result)
+def get_config_parameter(parameter_name, parameters, default):
+    if parameter_name in parameters:
+        return parameters[parameter_name]
+    return default
 
 
 def get_date_string():
@@ -45,6 +44,22 @@ def get_lappland_ip():
     # todo(rodney): add jq command here to extract ip address from
     # terraform output
     return '0.0.0.0'
+
+
+def get_random_server_name():
+    result = choices(string.ascii_lowercase, k=3)
+    for i in range(0, 7):
+        result.append(str(randint(0, 9)))
+    return ''.join(result)
+
+
+def get_ssh_key_name():
+    return './configs/id_ed25519_lappland_' + get_date_string()
+
+
+def get_ssh_public_key(filename):
+    f = open(filename + '.pub', 'r')
+    return str(f.readlines()[0])
 
 
 def get_vpn_peers(parameters, server_address):
@@ -89,44 +104,26 @@ def get_vpn_wg_subnet(server_address):
     return str(IPv4Network(str(server_address) + "/24", False))
 
 
-def get_ssh_key_name():
-    return './configs/id_ed25519_lappland_' + get_date_string()
-
-
-def get_ssh_public_key(filename):
-    f = open(filename + '.pub', 'r')
-    return str(f.readlines()[0])
-
-
 def load_config():
     with open(r'./config.yml') as file:
         parameters = yaml.load(file, Loader=yaml.FullLoader)
     return parameters
 
 
-def get_config_parameter(parameter_name, parameters, default):
-    if parameter_name in parameters:
-        return parameters[parameter_name]
-    return default
-
-
 def main():
     parameters = load_config()
     env_copy = os.environ.copy()
 
-    admin_account = 'lappland'
-    if 'admin_account' in parameters:
-        admin_account = parameters['admin_account']
+    admin_account = get_config_parameter(
+        'admin_account', parameters, 'lappland')
 
     if 'server_name' in parameters:
         server_name = parameters['server_name']
     else:
         server_name = get_random_server_name()
 
-    firewall_select_source = "0.0.0.0/0"
-    if 'firewall_select_source' in parameters:
-        firewall_select_source = parameters['firewall_select_source']
-
+    firewall_select_source = get_config_parameter(
+        'firewall_select_source', parameters, "0.0.0.0/0")
     if 'gcloud' in parameters:
         region = parameters['gcloud']['region']
 
@@ -165,14 +162,18 @@ def main():
     env_copy['LAPPLAND_SERVER_IP'] = get_lappland_ip()
     env_copy['MAIL_CLIENTS'] = get_config_parameter(
         'mail_clients', parameters, "0.0.0.0/0")
-    env_copy['SSH_PRIVATE_KEY_FILE'] = get_ssh_key_name()
-    env_copy['SSH_CLIENTS'] = get_config_parameter(
+    ssh_private_key_file = get_ssh_key_name()
+    env_copy['SSH_PRIVATE_KEY_FILE'] = ssh_private_key_file
+    ssh_clients = get_config_parameter(
         'ssh_clients', parameters, firewall_select_source)
-    env_copy['WIREGUARD_PEERS'] = get_config_parameter(
+    env_copy['SSH_CLIENTS'] = ssh_clients
+    wireguard_peers = get_config_parameter(
         'wireguard_peers', parameters, firewall_select_source)
+    env_copy['WIREGUARD_PEERS'] = wireguard_peers
     wireguard_address = get_vpn_wg_server_address()
     env_copy['WIREGUARD_SERVER_ADDRESS'] = wireguard_address
-    env_copy['WIREGUARD_SUBNET'] = get_vpn_wg_subnet(wireguard_address)
+    wireguard_subnet = get_vpn_wg_subnet(wireguard_address)
+    env_copy['WIREGUARD_SUBNET'] = wireguard_subnet
     env_copy['PEERS'] = get_vpn_peers(parameters, wireguard_address)
 
     response = input("Do you accept wgcf Terms of Service? (yes/no) ")
@@ -181,6 +182,20 @@ def main():
 
     command = ['sh', 'server.sh']
     subprocess.check_call(command, env=env_copy)
+
+    # update lappland properties file
+    properties = {
+      'lappland_server_name': server_name,
+      'ssh_clients': ssh_clients,
+      'ssh_port': str(parameters['ssh_port']),
+      'ssh_private_key_file': ssh_private_key_file,
+      'wireguard_peers': wireguard_peers,
+      'wireguard_port': str(parameters['wg_port']),
+      'wireguard_server_address': wireguard_address,
+      'wireguard_subnet': wireguard_subnet,
+    }
+    with open('configs/properties.yml', 'w') as properties_file:
+        yaml.dump(properties, properties_file)
 
 
 if __name__ == "__main__":
