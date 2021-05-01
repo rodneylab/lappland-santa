5d3c2d3b1231ce13470340f1e23233d307108050
Rodney Lab Codesign <codesign@rodneylab.com>
2021-05-01 16:32:43 +0100

    fix(roles/ids): 🔧 made suricata rules update more robust

--- roles/ids/templates/suricata-rules-update.sh
+++ roles/ids/templates/suricata-rules-update.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 /usr/local/bin/oinkmaster -C /etc/oinkmaster.conf -o /etc/suricata/rules
 /usr/sbin/rcctl restart suricata
+sleep 60
+RETRIES=0
+until [ $RETRIES -eq 60 ] || /etc/rc.d/suricata check; do
+  rm -f /var/suricata/run/suricata.pid
+  /etc/rc.d/suricata restart
+  sleep 60
+done
